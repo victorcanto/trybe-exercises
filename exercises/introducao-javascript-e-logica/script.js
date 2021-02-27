716bf9d7dcf56e06f1d075a6561bc0d035b58aae
Victor Canto <iamvictorcanto@gmail.com>
2021-02-26 23:26:33 -0300

    Update file

---
+++ exercises/introducao-javascript-e-logica/script.js
@@ -0,0 +1,66 @@
+// JavaScript - Primeiros passos - Exercicios - ParteII
+
+// const name = 'Victor Canto';
+// const birthCity = 'Paudalho'
+// let birthYear = 1999;
+//   console.log(`Meu nome é ${name}, e nasci no ano de ${birthYear}.`);
+
+// birthYear = 2020;
+//   console.log(birthYear);
+
+// // birthCity = 'Carpina';
+// console.log(birthCity);
+
+// Resposta do item 6 : Por que o tipo de variável const não pode ser alterado, pois como o nome já diz, é uma constante.
+
+// JavaScript - Primeiros passos - Exercicios - ParteIII
+
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+//   console.log(typeof patientAge);
+//   console.log(typeof patientId);
+
+// Exercicio da parteIII
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = (base * 2) + (altura * 2);
+
+// console.log(`A base do retangulo é: ${base}`);
+// console.log(`A altura do retangulo é: ${altura}`);
+// console.log(`A área do retangulo é: ${area}`);
+// console.log(`O perimetro do retangulo é: ${perimetro}`);
+
+// Exercicio da parteIV
+// let notaDoCandidato = 59;
+// if (notaDoCandidato >= 80) {
+//   console.log('Parabéns, você foi aprovada(o)');
+// } else if(notaDoCandidato < 80 && notaDoCandidato >=60) {
+//   console.log('Você está na nossa lista de espera');
+// } else {
+//   console.log('Você foi reprovada(o)');
+// };
+
+// Exercicio da parteV
+// let statePeople = "aprovada";
+// switch (statePeople) {
+//   case "aprovada":
+//     console.log("aprovada");
+//     break;
+
+//   case "lista":
+//     console.log("lista");
+//     break;
+
+//   case "reprovada":
+//     console.log("reprovada");
+//     break;
+
+//   default:
+//     console.log("não se aplica");
+// }
