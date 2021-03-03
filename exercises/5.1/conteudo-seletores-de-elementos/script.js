5eb7395104f413436d3724e5a5fdaa9606c598f6
Victor Canto <iamvictorcanto@gmail.com>
2021-03-03 20:36:25 -0300

    Exercicios de seletores concluido

---
+++ exercises/5.1/conteudo-seletores-de-elementos/script.js
@@ -0,0 +1,17 @@
+let paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+let pageTitle = document.getElementById("page-title");
+pageTitle.innerHTML = "<h2>Matrix</2>";
+
+let secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.innerHTML = "<p>O melhor sobre robos da época.</p>";
+
+let subtitle = document.getElementById("subtitle");
+subtitle.innerHTML = "<h4>I'am Neo!</h4>";
+
+let bGreenItalic = document.getElementsByClassName("bgreen-italic");
+bGreenItalic[0].style.backgroundColor = "blue";
+
+let subtitleTag = document.getElementsByTagName("h4");
+subtitleTag[0].innerHTML = "<h4>My name is Neo!</h4>";
