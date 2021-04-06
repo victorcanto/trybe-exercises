c00ade1e5cee062fc5079c8ad328f06a479dbece
Victor Canto <iamvictorcanto@gmail.com>
2021-04-06 18:07:40 -0300

    exercise3 finalizado

---
+++ exercises/8.1/exercise3.js
@@ -0,0 +1,11 @@
+// Sugestão de respostas a serem validadas.
+const correctAnswer = "higher order function";
+const userAnswer = "HIGHER ORDER FUNCTION";
+
+const checkAnswer = (feedback, personResponse) => {
+  const answer = personResponse.toLowerCase();
+
+  return feedback === answer ? true : false;
+};
+
+console.log(checkAnswer(correctAnswer, userAnswer));
