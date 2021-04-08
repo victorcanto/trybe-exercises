fe483f05112a5772aac5256933c405bb03a9bf61
Victor Canto <iamvictorcanto@gmail.com>
2021-04-08 03:44:39 -0300

    exercise4 finalizado

---
+++ exercises/8.5/exercise4.js
@@ -0,0 +1,49 @@
+const assert = require("assert");
+
+const people = [
+  {
+    name: "Nicole",
+    bornIn: 1992,
+    nationality: "Australian",
+  },
+  {
+    name: "Harry",
+    bornIn: 2008,
+    nationality: "Australian",
+  },
+  {
+    name: "Toby",
+    bornIn: 1901,
+    nationality: "Australian",
+  },
+  {
+    name: "Frida",
+    bornIn: 1960,
+    nationality: "Dannish",
+  },
+  {
+    name: "Fernando",
+    bornIn: 2001,
+    nationality: "Brazilian",
+  },
+];
+
+const filterPeople = (arrPeople) => {
+  const filter = people.filter(({ bornIn, nationality }) => {
+    return bornIn >= 1901 && bornIn <= 2000 && nationality === "Australian";
+  });
+  return filter;
+};
+
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], {
+  name: "Nicole",
+  bornIn: 1992,
+  nationality: "Australian",
+});
+assert.deepStrictEqual(filteredPeople[1], {
+  name: "Toby",
+  bornIn: 1901,
+  nationality: "Australian",
+});
