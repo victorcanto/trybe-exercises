6a093b30afd008622574fb21189b9a4e71abf208
Victor Canto <iamvictorcanto@gmail.com>
2021-04-14 02:06:24 -0300

    Exercicio 2 R1 finalizado

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise2.js
@@ -0,0 +1,30 @@
+const lesson1 = {
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
+
+const lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
+
+const lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
+
+let newKey = "turno";
+const turno = "manhã";
+
+const addMorningShift = (obj, key, value) => {
+  obj[key] = value;
+  return obj;
+};
+
+console.log(addMorningShift(lesson2, newKey, turno));
+
