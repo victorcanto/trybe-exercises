59cb201e94f2d5f33cff62d91c229d2cb9ce31a6
Victor Canto <iamvictorcanto@gmail.com>
2021-04-14 03:08:39 -0300

    Exercicio 2 R8 finalizado

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise2.js
@@ -29,34 +29,46 @@ console.log(addMorningShift(lesson2, newKey, turno));
 
 const listObjKeys = (obj) => {
   return Object.keys(obj);
-}
+};
 console.log(listObjKeys(lesson1));
 
 const showSizeObj = (obj) => {
   return Object.keys(obj).length;
-}
+};
 console.log(showSizeObj(lesson2));
 
 const listObjValues = (obj) => {
   return Object.values(obj);
-}
+};
 console.log(listObjValues(lesson3));
 
-const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 console.log(allLessons);
 
 const getTotalStudents = (obj) => {
   let total = 0;
-  const objKeys =  Object.keys(obj);
+  const objKeys = Object.keys(obj);
   for (index in objKeys) {
     total += obj[objKeys[index]].numeroEstudantes;
   }
   return total;
-}
+};
 console.log(getTotalStudents(allLessons));
 
 const getValueByNumber = (obj, index) => {
-  const objValue =  Object.values(obj);
+  const objValue = Object.values(obj);
   return objValue[index];
-}
+};
 console.log(getValueByNumber(lesson1, 0));
+
+const verifyPair = (obj, keyName, keyValue) => {
+  const objEntries = Object.entries(obj);
+  let isEqual = false;
+  for (let idx in objEntries) {
+    objEntries[idx][0] === keyName && objEntries[idx][1] === keyValue
+      ? (isEqual = true)
+      : isEqual;
+  }
+  return isEqual;
+};
+console.log(verifyPair(lesson3, "turno", "noite"));
