440820587225646c91c0400f974392e7035e5fe0
Victor Canto <iamvictorcanto@gmail.com>
2021-04-15 09:13:42 -0300

    Exercise1 completed

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercises-part-2-tdd/exercise1.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+const addOne = (myArray) => {
+  return myArray.map((item) => item + 1);
+};
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
+
+assert.strictEqual(typeof addOne, "function");
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
