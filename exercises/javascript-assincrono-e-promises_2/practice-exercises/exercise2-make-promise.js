b813ce01003a6d59ee3bd2d8301bb1563786d7c6
Victor Canto <iamvictorcanto@gmail.com>
2021-04-15 23:06:29 -0300

    Requirement bonus completed

--- exercises/javascript-assincrono-e-promises_2/practice-exercises/exercise2-make-promise.js
+++ exercises/javascript-assincrono-e-promises_2/practice-exercises/exercise2-make-promise.js
@@ -1,25 +1,29 @@
-const fetchPromise = () => {
-  const myPromise = new Promise((resolve, reject) => {
-    const array = [];
-
-    const randomNumber = () => Math.floor(Math.random() * 50) + 1;
-    for (let index = 0; index < 10; index += 1) {
-      array.push(randomNumber());
-    }
-
-    const sum = array
-      .map((number) => number * number)
-      .reduce((acc, currentNumber) => acc + currentNumber);
-
-    sum < 8000 ? resolve(sum) : reject(sum);
-  });
-  const division = [2, 3, 5, 10];
-  myPromise
-    .then((sum) => division.map((number) => sum / number))
-    .then((arr) => arr.reduce((acc, cur) => acc + cur), 0)
-    .catch(() =>
-      console.log("É mais de oito mil! Essa promise deve estar quebrada!")
-    );
+const sumRandomNumber = () => {
+  const array = [];
+  const randomNumber = () => Math.floor(Math.random() * 50) + 1;
+  for (let index = 0; index < 10; index += 1) {
+    array.push(randomNumber());
+  }
+  const sum = array
+    .map((number) => number * number)
+    .reduce((acc, currentNumber) => acc + currentNumber);
+  if (sum >= 8000) throw new Error();
+  return sum;
 };
+
+const sumArrFromSum = (sum) => {
+  const division = [2, 3, 5, 10];
+  return division.map((number) => sum / number).reduce((acc, cur) => acc + cur);
+};
+
+const fetchPromise = async () => {
+  try {
+    const sum = await sumRandomNumber();
+    const sumFromSum = await sumArrFromSum(sum);
+  }
+  catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!');
+  }
+}
 
 fetchPromise();
