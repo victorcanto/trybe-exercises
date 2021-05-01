e42d029e9949e55ff9a799f2e75e52038368c441
Victor Canto <iamvictorcanto@gmail.com>
2021-04-30 21:29:13 -0300

    R3 implemented

--- fundamentals/bloco_10/day_3/fixation-exercises/Math.test.js
+++ fundamentals/bloco_10/day_3/fixation-exercises/Math.test.js
@@ -14,3 +14,12 @@ test("Verifica a chamada e o retorno da função multiplicar", () => {
   expect(math.multiplicar).toHaveBeenCalled();
   expect(math.multiplicar(7, 9)).toBe(10);
 });
+
+test("Verifica a chamda, o retorno esperado e os paramentros passados", () => {
+  math.somar = jest.spyOn(math, "somar").mockImplementation((a, b) => a + b);
+
+  math.somar(7, 3);
+  expect(math.somar).toHaveBeenCalled();
+  expect(math.somar(7, 3)).toBe(10);
+  expect(math.somar).toHaveBeenCalledWith(7, 3);
+});
