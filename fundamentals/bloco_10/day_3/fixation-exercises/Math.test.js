c279c47ef2ec22acde39d5c24d0afc38c426ecaf
Victor Canto <iamvictorcanto@gmail.com>
2021-04-30 21:16:52 -0300

    R2 implemented

--- fundamentals/bloco_10/day_3/fixation-exercises/Math.test.js
+++ fundamentals/bloco_10/day_3/fixation-exercises/Math.test.js
@@ -6,3 +6,11 @@ test("Verifica se a funcção mockada foi chamada", () => {
   math.subtrair(5, 3);
   expect(math.subtrair).toHaveBeenCalled();
 });
+
+test("Verifica a chamada e o retorno da função multiplicar", () => {
+  math.multiplicar = jest.fn().mockReturnValue(10);
+
+  math.multiplicar(7, 9);
+  expect(math.multiplicar).toHaveBeenCalled();
+  expect(math.multiplicar(7, 9)).toBe(10);
+});
