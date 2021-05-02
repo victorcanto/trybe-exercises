5a52e289aff71871654ddd4bfd22348a0ac3c738
Victor Canto <iamvictorcanto@gmail.com>
2021-05-02 16:46:55 -0300

    fixation-exercise-2 implemented

--- frontend/bloco_11/day_2/fixation-exercises-11-2/src/App.js
+++ frontend/bloco_11/day_2/fixation-exercises-11-2/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import Image from './components/Image';
+import Order from './components/Order';
 
 function App() {
+  const headphone = {
+    id: 102,
+    user: 'cena@gmail.com',
+    product: 'Razer Headphone',
+    price: {
+      value: 99.99,
+      currency: 'dollars',
+    },
+  };
+
+  const energyDrink = {
+    id: 77,
+    user: 'cena@gmail.com',
+    product: 'Monster 500mL',
+    price: {
+      value: 9.99,
+      currency: 'dollars',
+    },
+  };
+
   return (
     <div className='App'>
-      <Image
-        source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
-        alternativeText='Cute cat staring'
-      />
+      <h1> Orders recently created </h1>
+      <Order order={headphone} />
+      <Order order={energyDrink} />
     </div>
   );
 }
