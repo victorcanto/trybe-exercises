c844a928241cb14bb6e689a7da3fe167bc42664f
Victor Canto <iamvictorcanto@gmail.com>
2021-05-02 23:50:41 -0300

    fixation-exercise part2 completed

--- frontend/bloco_12/day_1/fixation-exercises/fancy-buttons/src/App.js
+++ frontend/bloco_12/day_1/fixation-exercises/fancy-buttons/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import './App.css';
 
-const handleClick = (e) => {
-  const colorBr = {
-    red: 'vermelho',
-    blue: 'azul',
-    black: 'preto',
-  };
-  return console.log(`Você clicou no botão ${colorBr[e.target.value]}`)
+const colorBr = {
+  red: 'vermelho',
+  blue: 'azul',
+  black: 'preto',
 };
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    return console.log(`Você clicou no botão ${colorBr[e.target.value]}`);
+  }
   render() {
     return (
       <div className='buttons_container'>
-        <button className='red_button' onClick={handleClick} value='red'>
+        <button className='red_button' onClick={this.handleClick} value='red'>
           Red
         </button>
-        <button className='blue_button' onClick={handleClick} value='blue'>
+        <button className='blue_button' onClick={this.handleClick} value='blue'>
           Blue
         </button>
-        <button className='black_button' onClick={handleClick} value='black'>
+        <button
+          className='black_button'
+          onClick={this.handleClick}
+          value='black'
+        >
           Black
         </button>
       </div>
