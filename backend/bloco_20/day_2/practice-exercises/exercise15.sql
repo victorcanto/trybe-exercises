a8c45ce6ddf06b7f5abe4d9716c826f9ea9f7795
Victor Canto <iamvictorcanto@gmail.com>
2021-07-26 18:58:29 -0300

    feat: exercise15 done

---
+++ backend/bloco_20/day_2/practice-exercises/exercise15.sql
@@ -0,0 +1,2 @@
+--Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.", em que Number se refira a quantidade de cientistas.
+SELECT CONCAT('Existem ', COUNT(*), ' cientistas na tabela Scientists.') AS string FROM Scientists.Scientists;
