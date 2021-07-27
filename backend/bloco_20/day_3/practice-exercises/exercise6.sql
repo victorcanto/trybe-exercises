0395e49e62ef526ecc1ec4fed0889492fac39912
Victor Canto <iamvictorcanto@gmail.com>
2021-07-26 22:44:07 -0300

    feat: exercise6 done

---
+++ backend/bloco_20/day_3/practice-exercises/exercise6.sql
@@ -0,0 +1,2 @@
+--Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.
+SELECT * FROM PecasFornecedores.Fornecimentos WHERE Preco BETWEEN 15 AND 40 ORDER BY Preco ASC;
