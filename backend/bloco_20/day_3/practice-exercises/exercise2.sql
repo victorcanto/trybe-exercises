893ce9f05f17a022249f55b35fa8635a1b3c9d16
Victor Canto <iamvictorcanto@gmail.com>
2021-07-26 22:09:36 -0300

    feat: exercise2 done

---
+++ backend/bloco_20/day_3/practice-exercises/exercise2.sql
@@ -0,0 +1,2 @@
+--Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2 . Organize o resultado por ordem alfabética de fornecedor.
+SELECT * FROM PecasFornecedores.Fornecimentos WHERE code = 2 ;
