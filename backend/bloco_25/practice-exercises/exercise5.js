aef7ec5b851a6fa8febc7113d9eebfacf532eed3
Victor Canto <iamvictorcanto@gmail.com>
2021-08-18 21:10:46 -0300

    feat(practice): exercise5 done

---
+++ backend/bloco_25/practice-exercises/exercise5.js
@@ -0,0 +1,9 @@
+// Exercício 5: Agrupe os clientes por sexo . Retorne o total de clientes de cada sexo no campo total .
+db.clientes.aggregate([
+  {
+    $group: {
+      _id: '$sexo',
+      total: { $sum: 1 },
+    },
+  },
+]);
