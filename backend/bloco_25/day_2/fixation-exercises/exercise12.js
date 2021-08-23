6c577ad35d2ade97618272eebc2dd88510721371
Victor Canto <iamvictorcanto@gmail.com>
2021-08-23 17:48:08 -0300

    feat(fixation $addFields): exercise 12 done

---
+++ backend/bloco_25/day_2/fixation-exercises/exercise12.js
@@ -0,0 +1,11 @@
+// Utilizando o banco de dados storage , faça o seguinte exercício:
+// Calcule o valor total do estoque, considerando que cada produto valha o mesmo que seu preço de venda. Lembre-se da quantidade.
+db.products.aggregate([
+  {
+    $addFields: {
+      stock_total_value: {
+        $multiply: ['$sale_price', '$quantify'],
+      },
+    },
+  },
+]);
