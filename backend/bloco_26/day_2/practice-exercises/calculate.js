308e60957438a60bcbb29357a05e933671002157
Victor Canto <iamvictorcanto@gmail.com>
2021-08-30 16:38:24 -0300

    feat(ex3): async/await calculate function

---
+++ backend/bloco_26/day_2/practice-exercises/calculate.js
@@ -0,0 +1,41 @@
+function calculate(a, b, c) {
+  return new Promise((resolve, reject) => {
+    const condition = [...arguments].some((arg) => typeof arg === 'string');
+    if (condition) reject(new Error('Informe apenas números'));
+
+    const calcResult = (a + b) * c;
+
+    if (calcResult < 50) reject(new Error('Valor muito baixo'));
+
+    resolve(calcResult);
+  });
+}
+
+// function callCalculate() {
+//   const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+
+//   calculate(...randomNumbers)
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err));
+// }
+
+// callCalculate();
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+// Async/Await
+
+async function callCalculate() {
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+
+  try {
+    const result = await calculate(...randomNumbers);
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+callCalculate();
