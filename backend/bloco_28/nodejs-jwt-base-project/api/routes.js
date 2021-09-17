8cea4870f1a504fff0c211e05cd4ac10e38b667a
victorcanto <78617543+victorcanto@users.noreply.github.com>
2021-09-17 00:17:49 -0300

    Delete backend/bloco_28/nodejs-jwt-base-project directory

--- backend/bloco_28/nodejs-jwt-base-project/api/routes.js
+++
@@ -1,9 +0,0 @@
-const getPosts = require('../controllers/posts');
-const createUsers = require('../controllers/createUser');
-const login = require('../controllers/login');
-
-module.exports = {
-  getPosts,
-  createUsers,
-  login,
-};
